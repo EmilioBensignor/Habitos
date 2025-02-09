cc12c6b2d21c62ef7cafc4c8ac6675ed15e4825e
Emilio Bensignor <liobensignor@gmail.com>
2025-02-09 13:32:17 -0300

    Habitos en index

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,12 @@
         <section v-if="user">
             <h1>Hiciste tus habitos?</h1>
             <p>Email: {{ user.email }}</p>
+            <p>Leer 1 pagina</p>
+            <p>Ver 1 clase</p>
+            <p>Escribir 1 commit</p>
+            <p>Ejercicios de kinesio</p>
+            <p>Salir afuera</p>
+            <p>Producto del pelo</p>
             <button @click="signOut">Cerrar Sesión</button>
         </section>
     </main>
