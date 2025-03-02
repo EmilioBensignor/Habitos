3f62d1841043dfde4606596d77e5c334f62ae16b
Emilio Bensignor <liobensignor@gmail.com>
2025-03-02 13:59:30 -0300

    Logout

--- components/default/Header.vue
+++ components/default/Header.vue
@@ -5,5 +5,25 @@
             <NuxtLink to="/">Habitos</NuxtLink>
             <NuxtLink to="/track">Seguimiento Diario</NuxtLink>
         </nav>
+        <button @click="signOut">Cerrar sesión</button>
     </header>
 </template>
+
+<script setup>
+definePageMeta({
+    middleware: 'auth'
+})
+const user = useSupabaseUser();
+const client = useSupabaseClient();
+const router = useRouter();
+
+async function signOut() {
+    try {
+        const { error } = await client.auth.signOut();
+        if (error) throw error;
+        router.push('/login')
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+</script>
