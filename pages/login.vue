e44dcdb11e2da671b16050967a72691d00a4efe0
Lio <liobensignor@gmail.com>
2025-03-13 16:27:32 -0300

    Fix login

--- pages/login.vue
+++ pages/login.vue
@@ -80,7 +80,14 @@ h1 {
     font-weight: bold;
 }
 
+form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
 .form-group {
+    width: 100%;
     margin-bottom: 1rem;
 }
 
@@ -111,6 +118,7 @@ button {
     border: none;
     border-radius: 4px;
     cursor: pointer;
+    margin-bottom: 1rem;
 }
 
 button:hover {
